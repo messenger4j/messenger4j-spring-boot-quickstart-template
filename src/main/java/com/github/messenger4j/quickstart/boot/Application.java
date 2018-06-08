64fd7d60d7160936347e0f0a6ea48eecea0df3c1
Max Grabenhorst <max@grabenhorst-consulting.de>
2018-06-08 20:23:27 +0200

    Upgrade to messenger4j 1.0.0 final

--- src/main/java/com/github/messenger4j/quickstart/boot/Application.java
+++ src/main/java/com/github/messenger4j/quickstart/boot/Application.java
@@ -1,8 +1,6 @@
 package com.github.messenger4j.quickstart.boot;
 
 import com.github.messenger4j.Messenger;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,8 +18,6 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootApplication
 public class Application {
-
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     @Bean
     public Messenger messenger(@Value("${messenger4j.pageAccessToken}") String pageAccessToken,
